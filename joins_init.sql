f3dbae828d3737d57179db1f27a8256d810690c8
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2019-11-19 14:51:14 -0500

    Added examples for lecture 20 (#1)
    - adding notebooks on ORM, MongoDB, SQL + MongoDB analytics

---
+++ joins_init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE animals (id INTEGER PRIMARY KEY, name VARCHAR(64), type VARCHAR(64));
+
+CREATE TABLE diets (id INTEGER PRIMARY KEY, animal VARCHAR(64), food VARCHAR(64));
+
+
+-- insert some stuff into the tables
